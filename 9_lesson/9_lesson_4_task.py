d4b57fa67f7e41ae13d8e381f4db5c80e49feae1
kozheshkurt <vkozeskurt@gmail.com>
2024-04-24 16:41:47 +0300

    add completed lesson 9

--- 9_lesson/9_lesson_4_task.py
+++ 9_lesson/9_lesson_4_task.py
@@ -7,6 +7,61 @@
 '''
 from abc import ABC, abstractmethod
 
-class Scanable(ABC):
+class Scannung(ABC):
 
     @abstractmethod
+    def scanen(self):
+        pass
+
+
+class Druckung(ABC):
+
+    @abstractmethod
+    def drucken(self):
+        pass
+
+
+class NetworkPrinter(Scannung, Druckung):
+    
+    def __init__(self, name, users_number):
+        self.users_number = users_number
+        self.name = name
+    
+    def scanen(self, user_number):
+        if user_number > self.users_number:
+            print('You are in the queue, please wait')
+        else:
+            print('Scanning goes. Take back your document')
+
+    def drucken(self, user_number):
+        if user_number > self.users_number:
+            print('You are in the queue, please wait')
+        else:
+            print('Printing goes. Take back your document')
+
+
+
+class Printer(Druckung):
+
+    def __init__(self, name):
+        self.name = name
+
+    def drucken(self):
+        print(f'Printing on the {self.name} goes')
+
+
+class Scanner(Scannung):
+
+    def __init__(self, name):
+        self.name = name
+
+    def scanen(self):
+        print(f'Scanning on the {self.name} goes')
+
+
+scanner = Scanner('Samsung')
+scanner.scanen()
+
+networkprinter = NetworkPrinter('Hp', 3)
+networkprinter.drucken(4)
+networkprinter.drucken(3)
--- 9_lesson/9_lesson_5_task.py
+++ 9_lesson/9_lesson_5_task.py
@@ -4,3 +4,72 @@
 використовував абстракції та інтерфейси замість конкретних реалізацій. Переконайтеся,
 що класи залежностей не знають про конкретну реалізацію інших класів.
 '''
+from abc import ABC, abstractmethod
+
+class Scannung(ABC):
+
+    @abstractmethod
+    def scanen(self):
+        pass
+
+
+class Druckung(ABC):
+
+    @abstractmethod
+    def drucken(self):
+        pass
+
+
+class NetworkPrinter(Scannung, Druckung):
+    
+    def __init__(self, name, users_number):
+        self.users_number = users_number
+        self.name = name
+    
+    def scanen(self, user_number):
+        if user_number > self.users_number:
+            print('You are in the queue, please wait.')
+        else:
+            print('Scanning goes. Take back your document.')
+
+    def drucken(self, user_number):
+        if user_number > self.users_number:
+            print('You are in the queue, please wait.')
+        else:
+            print('Printing goes. Take back your document.')
+
+
+
+class Printer(Druckung):
+
+    def __init__(self, name):
+        self.name = name
+
+    def drucken(self):
+        print(f'Printing on the {self.name} goes.')
+
+
+class Scanner(Scannung):
+
+    def __init__(self, name):
+        self.name = name
+
+    def scanen(self):
+        print(f'Scanning on the {self.name} goes.')
+
+
+class PowerManage:
+
+    def __init__(self, device):
+        self.device = device
+    
+    def power_on(self):
+        print(f'Your {self.device.name} is ON.')
+
+    def power_off(self):
+        print(f'Your {self.device.name} is OFF.')
+
+
+network_printer = NetworkPrinter('Samsung', 3)
+network_printer_power = PowerManage(network_printer)
+network_printer_power.power_on()
